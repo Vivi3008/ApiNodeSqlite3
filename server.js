49a7c29a9efaf0870c1c0d51d440d45e023e1e0f
vivi <financeiro@portalautopecas.com.br>
2020-12-24 11:00:09 -0300

    add procfile

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
 
@@ -6,4 +9,4 @@ app.use(express.json())
 
 app.use('/api', require('./routes'))
 
-app.listen('3000')
+app.listen(proces.env.PORT || 3000)
